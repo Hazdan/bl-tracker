a137822cc27d089da65cc0b6a4a0591a31c161c4
Hazdan <me.manuelromero@gmail.com>
2022-11-08 21:59:06 -0500

    Set dockerfile with requirements and configure endpoints in sanic apps

--- api/src/server.py
+++ api/src/server.py
@@ -1,9 +1,16 @@
 from sanic import Sanic
 from sanic.response import text
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic("API")
 
 @app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+async def hello_api(request):
+    return text("Hello, from api.")
  
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=3000,
+        debug=True,
+        auto_reload=True
+    )
--- scrapper/src/server.py
+++ scrapper/src/server.py
@@ -1,9 +1,17 @@
 from sanic import Sanic
 from sanic.response import text
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic("SCRAPPER")
 
 @app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+async def hello_scrapper(request):
+    return text("Hello, from scrapper.")
  
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5500,
+        debug=True,
+        auto_reload=True
+    )
